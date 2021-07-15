e3a583a66b416dd962bff5c5f3a44daa3836072e
young <>
2021-07-15 22:42:58 +0900

    9th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like helloWorld hello!');
+    res.send('Please do not stop the music');
 });
 
 app.get('/start', function(req, res){
