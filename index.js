88b31c57bcbf88cf7ef637b0e7c5ebbe43db44dc
young <>
2021-07-15 22:31:03 +0900

    7th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like sushi!');
+    res.send('I like helloWorld!');
 });
 
 app.get('/start', function(req, res){
