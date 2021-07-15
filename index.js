604be4f9d50d20691d6b15efe5a2fcdfac405cf8
young <>
2021-07-15 22:21:50 +0900

    5th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Young BTS Black Pink!');
+    res.send('I like Young IU Pink!');
 });
 
 app.get('/start', function(req, res){
