fd6de04c3b7fbd39c5fadde1524becb1839a0c78
young <>
2021-07-15 22:17:02 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Young man!');
+    res.send('I like Young BTS!');
 });
 
 app.get('/start', function(req, res){
